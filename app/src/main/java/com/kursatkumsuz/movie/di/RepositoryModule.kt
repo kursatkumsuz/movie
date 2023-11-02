c5ecec1bdd4518256fcccf0eab37ddc50a1c9f34
DrHistory <kursatkumsuz@gmail.com>
2023-11-02 22:40:26 +0300

    Add Movie Repository

--- app/src/main/java/com/kursatkumsuz/movie/di/RepositoryModule.kt
+++ app/src/main/java/com/kursatkumsuz/movie/di/RepositoryModule.kt
@@ -2,10 +2,13 @@ package com.kursatkumsuz.movie.di
 
 import android.content.Context
 import com.google.firebase.auth.FirebaseAuth
+import com.kursatkumsuz.movie.data.remote.MovieApiService
 import com.kursatkumsuz.movie.data.repository.AuthenticationRepositoryImpl
 import com.kursatkumsuz.movie.data.repository.DataStoreRepositoryImpl
+import com.kursatkumsuz.movie.data.repository.movie.MovieRepositoryImpl
 import com.kursatkumsuz.movie.domain.repository.AuthenticationRepository
 import com.kursatkumsuz.movie.domain.repository.DataStoreRepository
+import com.kursatkumsuz.movie.domain.repository.movie.MovieRepository
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
@@ -27,6 +30,12 @@ object RepositoryModule {
     @Singleton
     fun provideAuthenticationRepository(firebaseAuth: FirebaseAuth): AuthenticationRepository {
         return AuthenticationRepositoryImpl(firebaseAuth = firebaseAuth)
+    }
+
+    @Provides
+    @Singleton
+    fun provideMovieRepository(movieApiService: MovieApiService): MovieRepository {
+        return MovieRepositoryImpl(movieApiService)
     }
 
 }
