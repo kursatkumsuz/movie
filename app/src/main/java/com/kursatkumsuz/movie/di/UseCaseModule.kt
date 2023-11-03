ae1d06bdd7938e76b5ac45887c963eb43598440a
DrHistory <kursatkumsuz@gmail.com>
2023-11-03 09:48:19 +0300

    Update UseCaseModule & UseCases Data Class

--- app/src/main/java/com/kursatkumsuz/movie/di/UseCaseModule.kt
+++ app/src/main/java/com/kursatkumsuz/movie/di/UseCaseModule.kt
@@ -2,12 +2,16 @@ package com.kursatkumsuz.movie.di
 
 import com.kursatkumsuz.movie.domain.repository.AuthenticationRepository
 import com.kursatkumsuz.movie.domain.repository.DataStoreRepository
+import com.kursatkumsuz.movie.domain.repository.movie.MovieRepository
 import com.kursatkumsuz.movie.domain.usecase.UseCases
 import com.kursatkumsuz.movie.domain.usecase.authentication.SignInWithEmailAndPasswordUseCase
 import com.kursatkumsuz.movie.domain.usecase.authentication.SignUpWithEmailAndPasswordUseCase
 import com.kursatkumsuz.movie.domain.usecase.authentication.IsSignedInUseCase
 import com.kursatkumsuz.movie.domain.usecase.datastore.ReadOnBoardingStateUseCase
 import com.kursatkumsuz.movie.domain.usecase.datastore.SaveOnBoardingStateUseCase
+import com.kursatkumsuz.movie.domain.usecase.movie.GetNowPlayingMovieUseCase
+import com.kursatkumsuz.movie.domain.usecase.movie.GetPopularMovieUseCase
+import com.kursatkumsuz.movie.domain.usecase.movie.GetTopRatedMovieUseCase
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
@@ -21,14 +25,18 @@ object UseCaseModule {
     @Provides
     fun provideUseCases(
         dataStoreRepository: DataStoreRepository,
-        authenticationRepository: AuthenticationRepository
+        authenticationRepository: AuthenticationRepository,
+        movieRepository: MovieRepository
     ): UseCases {
         return UseCases(
             saveOnBoardingStateUseCase = SaveOnBoardingStateUseCase(dataStoreRepository),
             readOnBoardingStateUseCase = ReadOnBoardingStateUseCase(dataStoreRepository),
             signInWithEmailAndPasswordUseCase = SignInWithEmailAndPasswordUseCase(authRepository = authenticationRepository),
             signUpWithEmailAndPasswordUseCase = SignUpWithEmailAndPasswordUseCase(authRepository = authenticationRepository),
-            isSignedInUseCase = IsSignedInUseCase(authRepository = authenticationRepository)
+            isSignedInUseCase = IsSignedInUseCase(authRepository = authenticationRepository),
+            getTopRatedMovieUseCase = GetTopRatedMovieUseCase(movieRepository = movieRepository),
+            getPopularMovieUseCase = GetPopularMovieUseCase(movieRepository = movieRepository),
+            getNowPlayingMovieUseCase = GetNowPlayingMovieUseCase(movieRepository)
         )
     }
 }
--- app/src/main/java/com/kursatkumsuz/movie/domain/usecase/UseCases.kt
+++ app/src/main/java/com/kursatkumsuz/movie/domain/usecase/UseCases.kt
@@ -5,11 +5,17 @@ import com.kursatkumsuz.movie.domain.usecase.authentication.SignUpWithEmailAndPa
 import com.kursatkumsuz.movie.domain.usecase.authentication.IsSignedInUseCase
 import com.kursatkumsuz.movie.domain.usecase.datastore.ReadOnBoardingStateUseCase
 import com.kursatkumsuz.movie.domain.usecase.datastore.SaveOnBoardingStateUseCase
+import com.kursatkumsuz.movie.domain.usecase.movie.GetNowPlayingMovieUseCase
+import com.kursatkumsuz.movie.domain.usecase.movie.GetPopularMovieUseCase
+import com.kursatkumsuz.movie.domain.usecase.movie.GetTopRatedMovieUseCase
 
 data class UseCases(
     val saveOnBoardingStateUseCase: SaveOnBoardingStateUseCase,
     val readOnBoardingStateUseCase: ReadOnBoardingStateUseCase,
     val signInWithEmailAndPasswordUseCase: SignInWithEmailAndPasswordUseCase,
     val signUpWithEmailAndPasswordUseCase: SignUpWithEmailAndPasswordUseCase,
-    val isSignedInUseCase: IsSignedInUseCase
+    val isSignedInUseCase: IsSignedInUseCase,
+    val getTopRatedMovieUseCase: GetTopRatedMovieUseCase,
+    val getPopularMovieUseCase: GetPopularMovieUseCase,
+    val getNowPlayingMovieUseCase: GetNowPlayingMovieUseCase
 )
