6bc8fe43d3b4b961dd0a8ad7d21f3ca38ae85d8c
DrHistory <kursatkumsuz@gmail.com>
2023-11-03 09:12:21 +0300

    Create use cases for fetch movie data

---
+++ app/src/main/java/com/kursatkumsuz/movie/domain/usecase/movie/GetPopularMovieUseCase.kt
@@ -0,0 +1,15 @@
+package com.kursatkumsuz.movie.domain.usecase.movie
+
+import androidx.paging.PagingData
+import com.kursatkumsuz.movie.data.model.Result
+import com.kursatkumsuz.movie.domain.repository.movie.MovieRepository
+import kotlinx.coroutines.flow.Flow
+import javax.inject.Inject
+
+class GetPopularMovieUseCase @Inject constructor(
+    private val movieRepository: MovieRepository
+) {
+    suspend operator fun invoke(): Flow<PagingData<Result>> {
+        return movieRepository.getPopularMovies()
+    }
+}
