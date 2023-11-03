232dfb295edbe65b8a7c8a288031b3f68b96e520
DrHistory <kursatkumsuz@gmail.com>
2023-11-03 09:08:47 +0300

    Implement OkHttp Client

--- app/src/main/java/com/kursatkumsuz/movie/di/NetworkModule.kt
+++ app/src/main/java/com/kursatkumsuz/movie/di/NetworkModule.kt
@@ -2,10 +2,12 @@ package com.kursatkumsuz.movie.di
 
 import com.kursatkumsuz.movie.data.remote.MovieApiService
 import com.kursatkumsuz.movie.util.Constants.BASE_URL
+import com.kursatkumsuz.movie.util.interceptors.ErrorInterceptor
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import okhttp3.OkHttpClient
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import javax.inject.Singleton
@@ -14,17 +16,30 @@ import javax.inject.Singleton
 @InstallIn(SingletonComponent::class)
 object NetworkModule {
 
-
+    @Singleton
+    @Provides
+    fun provideErrorInterceptor() = ErrorInterceptor()
+    @Singleton
+    @Provides
+    fun provideOkHttp(
+        errorInterceptor: ErrorInterceptor
+    ): OkHttpClient {
+        return OkHttpClient.Builder()
+            .addInterceptor(errorInterceptor)
+            .build()
+    }
 
     @Singleton
     @Provides
-    fun provideApiService(retrofit: Retrofit): MovieApiService = retrofit.create(MovieApiService::class.java)
+    fun provideApiService(retrofit: Retrofit): MovieApiService =
+        retrofit.create(MovieApiService::class.java)
 
     @Singleton
     @Provides
-    fun provideRetrofit(): Retrofit = Retrofit
+    fun provideRetrofit(okHttpClient: OkHttpClient): Retrofit = Retrofit
         .Builder()
         .baseUrl(BASE_URL)
+        .client(okHttpClient)
         .addConverterFactory(GsonConverterFactory.create())
         .build()
 }
